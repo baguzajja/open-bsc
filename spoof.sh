d58fef84a44bb900d63c485f87f11f375cd95da8
Baguz A Wibowo <baguzkyu@gmail.com>
2024-03-09 06:13:06 +0700

    Update spoof.sh

--- spoof.sh
+++ spoof.sh
@@ -2,7 +2,7 @@
 
 gnome-terminal -- bash -c 'sudo python3 main_uhd_spoof.py -u --gprs -i wlp3s0 --sip; exec bash'
 
-gnome-terminal -- bash -c 'sudo osmo-trx-uhd -s 4 -c 1 -e -l INFO -f -C /usr/src/osmo-nitb-scripts/configs/osmo-trx-uhd.cfg; exec bash'
+gnome-terminal -- bash -c 'sudo osmo-trx-uhd -C /usr/src/osmo-nitb-scripts/configs/osmo-trx-uhd.cfg; exec bash'
 	
 echo "Aplikasi Running"
 
